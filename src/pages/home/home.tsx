821128af8e157d100a0e87e8fa73380b3568bfe6
zexhior <herizoras1@gmail.com>
2026-06-25 12:16:35 +0300

    feat: create section degrees

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,15 @@
 import Nav from "@/components/nav";
 import Header from "../header/header";
 import Skills from "../skills/skills";
+import Degrees from "../degrees/degrees";
 
 const Home = () => {
   return (
-    <div className="px-5 md:px-0 mx-auto w-full lg:w-290.5">
+    <div className="flex flex-col gap-10 px-5 md:px-0 mx-auto w-full lg:w-290.5">
       <Nav />
       <Header />
       <Skills />
+      <Degrees />
     </div>
   );
 };
