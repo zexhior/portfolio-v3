a6b8a23dbc70dff180ab11248866b4527a8b560d
zexhior <herizoras1@gmail.com>
2026-06-25 11:25:31 +0300

    feat: create section skills

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import Nav from "@/components/nav";
 import Header from "../header/header";
+import Skills from "../skills/skills";
 
 const Home = () => {
   return (
     <div className="px-5 md:px-0 mx-auto w-full lg:w-290.5">
       <Nav />
       <Header />
+      <Skills />
     </div>
   );
 };
