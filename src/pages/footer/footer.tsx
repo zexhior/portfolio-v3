2c612eeb244ca29e0cc0dcc2b51c4f62857846e3
zexhior <herizoras1@gmail.com>
2026-06-26 18:52:40 +0300

    style: add a little bit margin bottom to footer

--- src/pages/footer/footer.tsx
+++ src/pages/footer/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center text-slate-200">
+    <div className="flex justify-center items-center text-slate-200 mb-2">
       ©2026 Brice Herizo. Tous droits résérvés.
     </div>
   );
