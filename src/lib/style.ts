04cedcd8e6e2b663c3da5cfec47608198a5a564b
zexhior <herizoras1@gmail.com>
2026-06-27 14:41:09 +0300

    chore: create animation fade in custom and animation fade in right

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -1,4 +1,45 @@
 export const animationFadeIn = {
-  initial: { opacity: 0, translateY: 10 },
-  animate: { opacity: 1, translateY: 0, transition: { duration: 0.4, bounce: 5 } },
+  initial: { opacity: 0, y: 10 },
+  whileInView: { opacity: 1, y: 0 },
+  viewport: { once: true, amount: 0.5 },
+  transition: {
+    duration: 0.1,
+    opacity: { type: "spring", bounce: 0.5 },
+    y: { type: "spring", bounce: 0.5 },
+  },
+};
+
+export const animationFadeInRight = {
+  initial: { opacity: 0, x: 100 },
+  whileInView: { opacity: 1, x: 0 },
+  viewport: { once: true, amount: 0.5 },
+  transition: {
+    duration: 0.1,
+    opacity: { type: "spring", bounce: 0.5 },
+    x: { type: "spring", bounce: 0.5 },
+  },
+};
+
+export const animationFadeInHover = {
+  initial: { opacity: 0, y: 10 },
+  whileHover: { opacity: 1, y: 0 },
+  viewport: { once: true, amount: 0.5 },
+  transition: {
+    duration: 0.1,
+    opacity: { type: "spring", bounce: 0.5 },
+    y: { type: "spring", bounce: 0.5 },
+  },
+};
+
+export const animationFadeInCustom = ({ amount = 0.5, bounce = 0.5, type = "spring" }) => {
+  return {
+    initial: { opacity: 0, y: 10 },
+    whileInView: { opacity: 1, y: 0 },
+    viewport: { once: true, amount },
+    transition: {
+      duration: 0.1,
+      opacity: { type, bounce },
+      y: { type, bounce },
+    },
+  };
 };
