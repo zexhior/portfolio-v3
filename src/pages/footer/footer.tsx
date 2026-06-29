2d3a7d2ce7ef8c07463d6246ec72f5012146c445
zexhior <herizoras1@gmail.com>
2026-06-29 19:46:00 +0300

    feat: use data json inside footer

--- src/pages/footer/footer.tsx
+++ src/pages/footer/footer.tsx
@@ -1,9 +1,5 @@
-const Footer = () => {
-  return (
-    <div className="flex justify-center items-center text-slate-200 mb-2">
-      ©2026 Brice Herizo. Tous droits résérvés.
-    </div>
-  );
+const Footer = ({ footer }: { footer?: string }) => {
+  return <div className="flex justify-center items-center text-slate-200 mb-2">{footer}</div>;
 };
 
 export default Footer;
