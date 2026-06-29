1f803d4e8ac1d757b734e60db807e87a619e7895
zexhior <herizoras1@gmail.com>
2026-06-29 11:36:35 +0300

    style: make modal responsive

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -18,10 +18,7 @@ import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carouse
 import Autoplay from "embla-carousel-autoplay";
 import Fade from "embla-carousel-fade";
 
-const Modal = ({
-  project,
-  children,
-}: {
+interface IModal extends React.AllHTMLAttributes<HTMLDivElement> {
   project: {
     name: string;
     tech: { tools: string; icon: ReactNode }[];
@@ -29,23 +26,25 @@ const Modal = ({
     photo: string[];
   };
   children: ReactNode;
-}) => {
+}
+
+const Modal: React.FC<IModal> = ({ project, children, ...props }) => {
   return (
-    <Dialog>
+    <Dialog {...props}>
       <DialogTrigger className="w-full md:w-1/2 h-100" asChild>
         {children}
       </DialogTrigger>
       <DialogContent
-        className="sm:max-w-[80vw] h-[80vh] bg-secondary shadow-[0_0_10px] shadow-slate-200 transition-transform duration-500 ease-in-out"
+        className="sm:max-w-[80vw] h-[80vh] bg-secondary shadow-[0_0_10px] shadow-slate-200 transition-transform duration-500 ease-in-out px-5"
         showCloseButton={false}
       >
-        <div className="flex h-full">
-          <div className="w-2/3 h-[80dvh] px-5">
+        <div className="flex flex-col md:flex-row h-full overflow-hidden">
+          <div className="w-full md:w-2/3 h-[40dvh] md:h-[70dvh] md:px-2">
             <Carousel className="h-full w-full" plugins={[Fade(), Autoplay({ delay: 6000 })]}>
-              <CarouselContent className="h-[70dvh]">
+              <CarouselContent className="h-[30dvh] md:h-[70dvh]">
                 {project.photo.map((photo) => {
                   return (
-                    <CarouselItem className=" w-full h-full bg-primary">
+                    <CarouselItem key={photo} className=" w-full h-full bg-primary">
                       <img
                         src={photo}
                         alt={photo}
@@ -57,7 +56,7 @@ const Modal = ({
               </CarouselContent>
             </Carousel>
           </div>
-          <div className="w-1/3 flex flex-col gap-4 text-slate-200 text-outfit">
+          <div className="w-full md:w-1/3 flex flex-col gap-4 text-slate-200 text-outfit h-[40dvh] md:h-[70dvh] overflow-y-scroll scrollbar-thumb-blue-950">
             <h3 className="text-4xl text-slate-500 text-oswald">{project.name}</h3>
             <p>{project.description}</p>
             <div className="flex flex-wrap gap-2">
@@ -135,43 +134,41 @@ const Projects = () => {
       <div className="flex flex-col md:flex-row flex-wrap">
         {projects.map((project, index) => {
           return (
-            <div key={`${index}${project.name}`}>
-              <Modal project={project}>
-                <motion.div
-                  {...animationFadeInCustom({ amount: 0.3 })}
-                  className={`flex flex-col p-2 h-full`}
+            <Modal key={`${index}-${project.name}`} project={project}>
+              <motion.div
+                {...animationFadeInCustom({ amount: 0.3 })}
+                className={`flex flex-col p-2 h-full`}
+              >
+                <div
+                  className={`rounded-xl overflow-hidden shadow-[0_0_8px] shadow-slate-300/50 border border-slate-600 h-full aspect-video relative`}
                 >
-                  <div
-                    className={`rounded-xl overflow-hidden shadow-[0_0_8px] shadow-slate-300/50 border border-slate-600 h-full aspect-video relative`}
+                  <img
+                    src={project.photo[0]}
+                    alt={project.name}
+                    className="h-full w-full absolute inset-0 object-cover z-0"
+                  />
+                  <motion.div
+                    {...animationFadeInHover}
+                    className="flex flex-col justify-end items-start text-white z-10 relative h-full w-full p-10 bg-linear-to-b from-10% to-40% from-transparent to-slate-950/80"
                   >
-                    <img
-                      src={project.photo[0]}
-                      alt={project.name}
-                      className="h-full w-full absolute inset-0 object-cover z-0"
-                    />
-                    <motion.div
-                      {...animationFadeInHover}
-                      className="flex flex-col justify-end items-start text-white z-10 relative h-full w-full p-10 bg-linear-to-b from-10% to-40% from-transparent to-slate-950/80"
-                    >
-                      <h3 className="text-oswald text-2xl text-slate-100">{project.name}</h3>
-                      <div className="flex flex-wrap gap-2 my-2">
-                        {project.tech.map((tech) => {
-                          return (
-                            <div
-                              className="flex items-center gap-2 p-2 text-xs bg-primary rounded-md"
-                              key={`${project.name}-${tech.tools}`}
-                            >
-                              {tech.icon}
-                              {tech.tools}
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </motion.div>
-                  </div>
-                </motion.div>
-              </Modal>
-            </div>
+                    <h3 className="text-oswald text-2xl text-slate-100">{project.name}</h3>
+                    <div className="flex flex-wrap gap-2 my-2">
+                      {project.tech.map((tech) => {
+                        return (
+                          <div
+                            className="flex items-center gap-2 p-2 text-xs bg-primary rounded-md"
+                            key={`${project.name}-${tech.tools}`}
+                          >
+                            {tech.icon}
+                            {tech.tools}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </motion.div>
+                </div>
+              </motion.div>
+            </Modal>
           );
         })}
       </div>
