88f0f67464368d82cffc6b5c3092bb4339f18633
zexhior <herizoras1@gmail.com>
2026-06-29 19:46:15 +0300

    feat: add each data of section and link data to loading

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -10,23 +10,30 @@ import Aurora from "@/components/animation/aurora";
 import { Toaster } from "sonner";
 import Loader from "@/components/animation/loading";
 import { useEffect, useState } from "react";
+import { useGetData } from "@/hooks/data";
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [lang, setLang] = useState<"fr" | "en">("fr");
+  const [isLoadingExtra, setIsLoadingExtra] = useState<boolean>(true);
+  const { data, isLoading } = useGetData();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 10000);
+      setIsLoadingExtra(false);
+    }, 5000);
     return () => clearTimeout(timeout);
   }, []);
 
   return (
     <>
-      {isLoading ? (
+      {isLoading || isLoadingExtra ? (
         <Loader />
       ) : (
-        <div className={`${isLoading ? "h-screen overflow-hidden" : "overflow-visible"}`}>
+        <div
+          className={`${
+            isLoading && isLoadingExtra ? "h-screen overflow-hidden" : "overflow-visible"
+          }`}
+        >
           <Toaster />
           <Aurora
             colorStops={["#7cff67", "#B497CF", "#5227FF"]}
@@ -35,14 +42,14 @@ const Home = () => {
             speed={1}
           ></Aurora>
           <div className="flex flex-col gap-10 px-5 md:px-0 mx-auto w-full md:max-w-4/5 overflow-x-clip md:overflow-x-visible">
-            <Nav />
-            <Header />
-            <Skills />
-            <Degrees />
+            <Nav nav={data ? data[lang]?.nav : undefined} />
+            <Header header={data ? data[lang]?.header : undefined} />
+            <Skills skills={data ? data[lang]?.skills : undefined} />
+            <Degrees degrees={data ? data[lang]?.degrees : undefined} />
             <Projects />
-            <Experiences />
-            <Contacts />
-            <Footer />
+            <Experiences experiences={data ? data[lang].experiences : undefined} />
+            <Contacts contacts={data ? data[lang].contacts : undefined} />
+            <Footer footer={data ? data[lang].footer : undefined} />
           </div>
         </div>
       )}
