6db62b3e663ffab3ee214aa6edc5279cb442ecf0
zexhior <herizoras1@gmail.com>
2026-07-04 16:38:49 +0300

    chore: add feature send message api

--- src/hooks/message.tsx
+++ src/hooks/message.tsx
@@ -1,4 +1,4 @@
-import { client } from "@/lib/api";
+import { clientN8N } from "@/lib/api";
 import { useMutation, type UseMutationResult } from "@tanstack/react-query";
 
 interface Message {
@@ -11,7 +11,8 @@ export const useSendMessage = (): UseMutationResult<Message, unknown, Message> =
   return useMutation<Message, unknown, Message>({
     mutationKey: [],
     mutationFn: async (message: Message) => {
-      const result = await client.post<Message>("api/message/", message);
+      const result = await clientN8N.post<Message>("webhook-test/message", message);
+
       return result.data;
     },
   });
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 
 export const client = axios.create({ baseURL: import.meta.env.VITE_BASE_URL });
+
+export const clientN8N = axios.create({ baseURL: import.meta.env.VITE_BASE_URL_N8N });
